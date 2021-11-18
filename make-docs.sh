06559959f7932ee1f035870566560c3076cd9e90
Phil Freeman <paf31@cantab.net>
2021-11-17 22:42:59 -0800

    Updates for 0.1.1.0

--- make-docs.sh
+++ make-docs.sh
@@ -1,3 +1,3 @@
 stack haddock --no-haddock-deps
-cp -r $(stack path --local-install-root)/doc/dovetail-0.1.0.0/ docs/dovetail-0.1.0.0
-cp -r $(stack path --local-install-root)/doc/dovetail-aeson-0.1.0.0/ docs/dovetail-aeson-0.1.0.0
+cp -r $(stack path --local-install-root)/doc/dovetail-0.1.1.0/ docs/dovetail
+cp -r $(stack path --local-install-root)/doc/dovetail-aeson-0.1.0.0/ docs/dovetail-aeson
